ac1bedea19173bdf0580f4d806121ab582511da7
moxiaov587 <moxiaov587@users.noreply.github.com>
2025-08-23 02:18:50 +0000

    deploy: 73d11a224f4c691d292fd12c6cfb9acdfa30bd61

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "fe04f73d33d2e5be93ed88f93b81f034",
+const RESOURCES = {"flutter_bootstrap.js": "22d91a5fb8b95f760c5c2422f6f29b0e",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
